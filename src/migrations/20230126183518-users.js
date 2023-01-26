06d8823c97f2a8847aef88d90c2208b9559a9191
ViniciusRCampos <vinicius_uo@hotmail.com>
2023-01-26 16:42:28 -0300

    Feat: Migrations to Create Tables

---
+++ src/migrations/20230126183518-users.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users',{
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      },
+      display_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      }
+
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.dropTable('users');
+
+  }
+};
