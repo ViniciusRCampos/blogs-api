84fc01666a3f2d74ac5af03e90ad8b478d7f07ac
ViniciusRCampos <vinicius_uo@hotmail.com>
2023-01-27 18:06:18 -0300

    Feat: Model Category Creation

---
+++ src/models/Category.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const category = sequelize.define('Category',{
+    id: {
+      type: DataTypes.INTEGER, 
+      primaryKey: true,
+    },
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    // modelName: 'Category',
+    underscored: true,
+    timestamps: false,
+  });
+  return category;
+};
