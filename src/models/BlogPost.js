7ad0e2fcfd62a7f7214b3071de7a03652d8943a5
ViniciusRCampos <vinicius_uo@hotmail.com>
2023-01-27 19:38:25 -0300

    Feat: Created BlogPost Model

---
+++ src/models/BlogPost.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const blogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE
+  }, {
+    sequelize,
+    // modelName: 'BlogPost',
+    timestamp: false,
+    underscored: true,
+  })
+
+  blogPost.associate = (models) => {
+    blogPost.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'users',
+    })  
+  }
+  return blogPost;
+}
